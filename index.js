3799de464810f167e21fed24723e195b953e8305
Gareth Jones <jones258@gmail.com>
2020-01-05 18:36:06 +1300

    feat: add `eslint-plugin-eslint-comments` plugin

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 module.exports = {
   env: { es2017: true },
   plugins: [
-    'prettier'
-    //
+    'prettier', //
+    'eslint-comments'
   ],
   extends: [
     'eslint:recommended',
-    'plugin:prettier/recommended'
-    //
+    'plugin:prettier/recommended',
+    'plugin:eslint-comments/recommended'
   ],
   rules: {
-    //
+    'eslint-comments/no-unused-disable': 'error'
   }
 };

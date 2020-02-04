4dc360ec0d1753a203942bad49f71a90e7118aec
Gareth Jones <jones258@gmail.com>
2020-02-04 18:31:44 +1300

    chore: install `eslint-plugin-eslint-plugin`

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,8 +7,12 @@ const config = {
     sourceType: 'module',
     ecmaVersion: 2019
   },
-  plugins: ['@typescript-eslint/eslint-plugin', 'local'],
-  extends: ['./index.js', './@typescript-eslint.js'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'local', 'eslint-plugin'],
+  extends: [
+    './index.js',
+    './@typescript-eslint.js',
+    'plugin:eslint-plugin/recommended'
+  ],
   overrides: [
     { files: ['*.spec.*'], extends: ['./jest.js'] },
     {

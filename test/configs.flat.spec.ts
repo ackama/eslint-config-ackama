b841ca72e543cf255227a3580357320bdc2a6bcc
Gareth Jones <jones258@gmail.com>
2024-11-12 13:24:54 +1300

    refactor: deduplicate some helpers

--- test/configs.flat.spec.ts
+++ test/configs.flat.spec.ts
@@ -3,8 +3,13 @@ process.env.ESLINT_USE_FLAT_CONFIG = 'true';
 import * as parserTypeScriptESLint from '@typescript-eslint/parser';
 import ESLint from 'eslint';
 import { FlatESLint } from 'eslint/use-at-your-own-risk';
-import * as fs from 'fs';
 import semver from 'semver/preload';
+import {
+  configFiles,
+  determinePluginPackageName,
+  makeEnabledRulesWarn,
+  typeDeclarations
+} from './helpers';
 import packageJson from '../package.json';
 
 // flat config does not include any information about the package that provides
@@ -12,51 +17,6 @@ import packageJson from '../package.json';
 const packagesForParsers = new Map<unknown, string>([
   [parserTypeScriptESLint, '@typescript-eslint/parser']
 ]);
-
-// eslint-disable-next-line n/no-sync
-const configFiles = fs
-  .readdirSync('.', { withFileTypes: true })
-  .filter(
-    value =>
-      value.isFile() &&
-      value.name.endsWith('.js') &&
-      !value.name.startsWith('eslint.config.')
-  )
-  .map(value => value.name);
-
-// eslint-disable-next-line n/no-sync
-const typeDeclarations = fs.readFileSync('configs.d.ts', 'utf8');
-
-/**
- * Determines the canonical package name for the given eslint `plugin`,
- * that can be used to install the plugin using a package manager.
- *
- * Generally this is done by returning the plugin name with `eslint-plugin-`
- * appended to it (if the name does not already start with that string).
- *
- * @param {string} plugin
- *
- * @return {string}
- */
-const determinePluginPackageName = (plugin: string): string => {
-  if (plugin.startsWith('eslint-plugin-')) {
-    return plugin;
-  }
-
-  if (plugin.startsWith('@')) {
-    const [scope, name] = plugin.split('/');
-
-    let packageName = `${scope}/eslint-plugin`;
-
-    if (name) {
-      packageName += `-${name}`;
-    }
-
-    return packageName;
-  }
-
-  return `eslint-plugin-${plugin}`;
-};
 
 const requireConfig = (
   config: string
@@ -120,19 +80,6 @@ describe('package.json', () => {
     );
   });
 });
-
-const makeEnabledRulesWarn = (
-  value: ESLint.Linter.RuleEntry
-): ESLint.Linter.RuleEntry => {
-  if (Array.isArray(value)) {
-    return [
-      value[0] !== 'off' && value[0] !== 0 ? 'warn' : 'off',
-      ...(value.slice(1) as unknown[])
-    ];
-  }
-
-  return value !== 'off' && value !== 0 ? 'warn' : 'off';
-};
 
 describe('for each config file', () => {
   describe.each(configFiles)('%s config', configFile => {
--- test/configs.legacy.spec.ts
+++ test/configs.legacy.spec.ts
@@ -2,53 +2,14 @@ process.env.ESLINT_USE_FLAT_CONFIG = 'false';
 
 import ESLint from 'eslint';
 import { LegacyESLint } from 'eslint/use-at-your-own-risk';
-import * as fs from 'fs';
 import semver from 'semver/preload';
+import {
+  configFiles,
+  determinePluginPackageName,
+  makeEnabledRulesWarn,
+  typeDeclarations
+} from './helpers';
 import packageJson from '../package.json';
-
-// eslint-disable-next-line n/no-sync
-const configFiles = fs
-  .readdirSync('.', { withFileTypes: true })
-  .filter(
-    value =>
-      value.isFile() &&
-      value.name.endsWith('.js') &&
-      !value.name.startsWith('eslint.config.')
-  )
-  .map(value => value.name);
-
-const typeDeclarations = fs.readFileSync('configs.d.ts', 'utf8');
-
-/**
- * Determines the canonical package name for the given eslint `plugin`,
- * that can be used to install the plugin using a package manager.
- *
- * Generally this is done by returning the plugin name with `eslint-plugin-`
- * appended to it (if the name does not already start with that string).
- *
- * @param {string} plugin
- *
- * @return {string}
- */
-const determinePluginPackageName = (plugin: string): string => {
-  if (plugin.startsWith('eslint-plugin-')) {
-    return plugin;
-  }
-
-  if (plugin.startsWith('@')) {
-    const [scope, name] = plugin.split('/');
-
-    let packageName = `${scope}/eslint-plugin`;
-
-    if (name) {
-      packageName += `-${name}`;
-    }
-
-    return packageName;
-  }
-
-  return `eslint-plugin-${plugin}`;
-};
 
 const requireConfig = (
   config: string
@@ -109,19 +70,6 @@ describe('package.json', () => {
     );
   });
 });
-
-const makeEnabledRulesWarn = (
-  value: ESLint.Linter.RuleEntry
-): ESLint.Linter.RuleEntry => {
-  if (Array.isArray(value)) {
-    return [
-      value[0] !== 'off' ? 'warn' : 'off',
-      ...(value.slice(1) as unknown[])
-    ];
-  }
-
-  return value !== 'off' ? 'warn' : 'off';
-};
 
 describe('for each config file', () => {
   describe.each(configFiles)('%s config', configFile => {
